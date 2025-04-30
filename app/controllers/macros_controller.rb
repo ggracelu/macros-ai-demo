b7a7b90477c1c897d612eecbe8fccb8a83d90e2b
Grace Lu <155923103+ggracelu@users.noreply.github.com>
2025-04-30 00:03:29 +0000

    started using openai

--- app/controllers/macros_controller.rb
+++ app/controllers/macros_controller.rb
@@ -2,4 +2,18 @@ class MacrosController < ApplicationController
   def display_form
     render({ :template => "macros_templates/new_form"})
   end
+  
+  def calculate
+    @description = params.fetch("description_param")
+    @image = params.fetch("image_param")
+
+    @image_converted = DataURI.convert(@image)
+
+    c = OpenAI::Chat.new
+    c.system("You are an expert nutritionist. Estimate the macronutrients (carbohydrates, proteins, and fats) in grams, as well as total calories in kcal.")
+    c.user(@the_description)
+    @result = c.assistant!
+
+    render({ :template => "macros_templates/results"})
+  end
 end
